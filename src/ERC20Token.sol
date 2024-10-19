e6ee9237976af0f820530ccc0fafdef6888cc442
Gonzalo <gnkz@users.noreply.github.com>
2024-10-19 15:01:57 -0300

    feat: Add ERC20Permit to ERC20Token contract (#4)

--- src/ERC20Token.sol
+++ src/ERC20Token.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.4;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Token is ERC20, Ownable {
+contract ERC20Token is ERC20Permit, Ownable {
     constructor(string memory _name, string memory _symbol, uint256 _supply, address _supplyReceiver, address _owner)
-        Ownable(_owner)
         ERC20(_name, _symbol)
+        ERC20Permit(_name)
+        Ownable(_owner)
     {
         _mint(_supplyReceiver, _supply);
     }
